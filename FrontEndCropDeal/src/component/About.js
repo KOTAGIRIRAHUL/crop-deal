c9ecf7de86579947a0e0754ecee345307e08a41f
KOTAGIRIRAHUL <102713928+KOTAGIRIRAHUL@users.noreply.github.com>
2022-12-02 13:01:42 +0530

    Add files via upload

---
+++ FrontEndCropDeal/src/component/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import UserService from "../services/UserService";
+class About extends Component {
+    
+      render() {
+        return (
+          <div className="container">
+              <div>
+           <center><h3 style={{paddingTop:"100px"}}><strong>This is the site which helps us to directly buy the crops(vegetables/fruits) from the farmers</strong></h3></center>
+          </div>
+          </div>
+        );
+      }
+    }
+
+export default About
